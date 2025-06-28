89fa29212aab0fba3c42aa9f9defbd7e556b041a
duc19092005 <duc19092005d@gmail.com>
2025-06-28 15:50:02 +0700

    changes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,10 +6,31 @@ const name : string = "abcd"
 
 function App() {
   return (
+<<<<<<< HEAD
     <>
       <div>
       </div> 
     </>
+=======
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> hello.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React 1
+
+          NullAbble
+        </a>
+      </header>
+    </div>
+>>>>>>> main
   );
 }
 
