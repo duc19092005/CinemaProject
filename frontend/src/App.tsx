940295675684e7a764f630339c3bba33b56927b7
TRAN-DUC-110840 <23dh110840@st.huflit.edu.vn>
2025-07-06 17:48:38 +0700

    Fix_Branch

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -13,8 +13,6 @@ import { useNavigate } from "react-router-dom";
 
 
 
-const name : string = "abcd"
-
 function App() {
   const navigate = useNavigate();
   const handleListfilm = () => {
@@ -343,24 +341,6 @@ function App() {
       <footer className="pt-32">
         <Bottom />
       </footer>
-
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> hello.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React 1
-
-          NullAbble
-        </a>
-      </header>
     </div>
   );
 }
