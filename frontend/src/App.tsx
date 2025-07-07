7013720d0b2c5941f0af9071fa758de2cbdc44cb
Lu1702 <128778245+Lu1702@users.noreply.github.com>
2025-07-07 13:49:50 +0700

    chiaFolderRa

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,31 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Qlnhanvien from './QL_Nhanvien/homeQLNhanvien'
 
-const name : string = "abcd"
 
-function App() {
+function App(){
   return (
-
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> hello.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React 1
-
-          NullAbble
-        </a>
-      </header>
-    </div>
-  );
+    <div>
+    <Qlnhanvien/>
+  </div>
+  )
 }
 
 export default App;
